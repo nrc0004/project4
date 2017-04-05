31bc217cecb45afef4117c8897a92a637d31091c
Natasha Clark <Natasharclark@gmail.com>
2017-04-05 13:20:13 -0400

    working on seeding issue

--- db/seeds.js
+++ db/seeds.js
@@ -2,26 +2,25 @@
 const Program = require('./models.js').Program
 const Exercise = require('./models.js').Exercise
 const Blog = require('./models.js').Blog
+const seedData = require('./seedData.json')
+//
+Program.remove({}, () => {
+  Program.create(seedData, () => {
+    process.exit()
+  })
+})
 
-// const seedData = require('./seedData.json')
-//
-// Program.remove({}, () => {
-//   Program.create(seedData, () => {
-//     process.exit()
-//   })
-// })
-//
-// Exercise.remove({}, () => {
-//   Program.create(seedData, () => {
-//     process.exit()
-//   })
-// })
-//
-// Blog.remove({}, () => {
-//   Blog.create(seedData, () => {
-//     process.exit()
-//   })
-// })
+Exercise.remove({}, () => {
+  Program.create(seedData, () => {
+    process.exit()
+  })
+})
+
+Blog.remove({}, () => {
+  Blog.create(seedData, () => {
+    process.exit()
+  })
+})
 
 var leg = new Program({"name": "legs", "body": "thing"})
 leg.save()
@@ -29,7 +28,12 @@ leg.save()
 var squat = new Exercise({"name": "squats", "sets": 12, "reps": 3})
 squat.save()
 
+
+
 var blog1 = new Blog ({"title": "Blog1", "content": "All of the things"})
 blog1.save()
+
+leg.exercise.push(squat)
+leg.save()
 
 //to seed: 'node db/seeds.js' then run 'mongo', 'use memes_db', 'db.memes.find()' to check
